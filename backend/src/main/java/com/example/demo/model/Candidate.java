a1d187df4e5551da9772244cf94452c6740a442a
Dusan <proba@nesto.com>
2026-04-02 19:15:27 +0200

    Implement model classes

---
+++ backend/src/main/java/com/example/demo/model/Candidate.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "candidates")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Full name is mandatory")
+    @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
+    @Column(name ="full_name", nullable = false)
+    private String fullName;
+
+    @NotNull(message = "Date of birth is mandatory")
+    @Past(message = "Date of birth must be in the past")
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @NotBlank(message= "Contract is mandatory")
+    @Column(name = "contract_number", unique = true, nullable = false)
+    private String contractNumber;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+        name = "candidate_skills",
+        joinColumns = @JoinColumn(name = "candidate_id"),
+        inverseJoinColumns = @JoinColumn(name = "skill_id")
+    )
+    private Set<Skill> skills = new HashSet<>();
+    
+}
